e085c09baadc2b31c90a2a7a272c778cccc0f51c
DevonJamesWright <devonjamess.wright@gmail.com>
2020-01-18 10:56:51 -0500

    Project at 75%

--- lib/class_chooser.rb
+++ lib/class_chooser.rb
@@ -1,6 +1,9 @@
 require 'nokogiri'
+require 'pry'
+require 'open-uri'
 require_relative "class_chooser/version"
-
+require_relative 'scraper.rb'
+require_relative 'cli.rb'
 module ClassChooser
   class Error < StandardError; end
   # Your code goes here...
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,79 @@
-#!/usr/bin/env ruby
-require 'nokogiri'
-require_relative "class_chooser/version"
-require 'open-uri'
-require 'pry'
-
 class Scraper
-def character_classes
-site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dd/classes"
-doc = Nokogiri::HTML(open(site))
- starting_list =  doc.css(".item").each {|klass| puts klass.children[1].children[0].attributes["alt"].value}
- puts starting_list
-end 
+    def self.character_classes
+        site = "https://dnd.wizards.com/%3Cnolink%3E/classes"
+        doc = Nokogiri::HTML(open(site))
+        starting_list =  doc.css(".item").each {|klass| puts klass.children[1].children[0].attributes["alt"].value}
+        puts starting_list.each do |khlass| 
+            if khlass.include? ("div")
+            khlass.delete
+            end 
+        end 
+    end 
 
-def fighter_info
-site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/classes/fighter"
+    def self.fighter_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/classes/fighter"
 
-doc = Nokogiri::HTML(open(site))
-puts doc.css("#content").css("p")[1..3]
-binding.pry
-end 
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css("#content").css("p")[1..3]
+    end 
+    def self.wizard_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/classes/wizard"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css("#content").css("p")[1..3]
+    end 
+    def self.rouge_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/classes/rouge"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css("#content").css("p")[1..3]
+    end 
+    def self.cleric_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/classes/cleric"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css("#content").css("p")[1..3]
+    end 
+    def self.race_choices
+        site = "https://dnd.wizards.com/%3Cnolink%3E/races"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.item-content').css('span')
+    end 
+    def self.half_orc_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/halforc"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4]
+    end 
+    def self.human_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/human"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4]
+    end 
+    def self.elf_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/elf"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4]
+    end 
+    def self.dwarf_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/dwarf"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4]
+    end 
+    def self.halfling_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/halfing"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4] 
+    end 
+    def self.gnome_info
+        site = "https://dnd.wizards.com/dungeons-and-dragons/what-is-dnd/races/gnome"
+
+        doc = Nokogiri::HTML(open(site))
+        puts doc.css('.intro').css('p')[2..4]
+    end 
 end 
